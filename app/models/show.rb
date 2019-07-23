a2077c1267cd6b01286ff6b27e0f496b3b34aa74
github username <github email address>
2019-07-23 10:14:23 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map do |l|
+            "#{l.first_name} #{l.last_name}"
+        end
+    end
 end
